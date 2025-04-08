79a4f93e8b7b04710764793ea94c9106b234196c
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-08 15:19:52 +0900

    fix: tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-import { heroui } from '@heroui/react/plugin';
+import { heroui } from '@heroui/react';
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
