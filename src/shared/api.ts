aae6c3d5ed8336ef8cce24de3d19dc661c80b943
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-08 15:32:13 +0900

    add: shared api

---
+++ src/shared/api.ts
@@ -0,0 +1,17 @@
+import ky from 'ky'
+
+const BASE_URL = 'http://localhost:3000/api'
+
+const api = ky.create({
+  prefixUrl: BASE_URL
+})
+
+const apiProxy = {
+  get: async <T>(...args: Parameters<typeof api.get>) => await api.get<T>(...args).json(),
+  post: async <T>(...args: Parameters<typeof api.post>) => await api.post<T>(...args).json(),
+  put: async <T>(...args: Parameters<typeof api.put>) => await api.put<T>(...args).json(),
+  delete: async <T>(...args: Parameters<typeof api.delete>) => await api.delete<T>(...args).json(),
+  patch: async <T>(...args: Parameters<typeof api.patch>) => await api.patch<T>(...args).json(),
+}
+
+export default apiProxy
