410a104ae7d871c6302107912bf54cebbe596ab1
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-10 18:23:44 +0900

    add: formatter

---
+++ src/shared/lib/formatter.ts
@@ -0,0 +1,28 @@
+import dayjs from "./dayjs";
+
+/**
+ * Format date time to YYYY-MM-DD HH:mm:ss
+ * @param time - time in seconds
+ * @returns formatted date time
+ */
+export const formatDateTime = (time: number) => {
+  return dayjs(time * 1000).format("YYYY-MM-DD HH:mm:ss");
+}
+
+/**
+ * Format distance to km
+ * @param distance - distance in meters
+ * @returns formatted distance
+ */
+export const formatDistance = (distance: number) => {
+  return (distance / 1000).toFixed(2);
+}
+
+/**
+ * Format pace to mm:ss
+ * @param pace - pace in seconds per kilometer
+ * @returns formatted pace
+ */
+export const formatPace = (pace: number) => {
+  return dayjs.duration(pace, "seconds").format("mm:ss");
+}
