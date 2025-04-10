dba18524089702c0437d6c55e335815373c3f1db
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-10 21:51:21 +0900

    fix: modal

--- src/widgets/running-modal/ui/RunningModal.tsx
+++ src/widgets/running-modal/ui/RunningModal.tsx
@@ -7,9 +7,13 @@ import SuspenseRunningDetail from "@features/running/ui/SuspenseRunningDetail"
 import RunningForm from "@features/running/ui/RunningForm"
 import AsyncBoundary from "@shared/ui/AsyncBoundary"
 import RunningListSkeleton from "@features/running/ui/RunningListSkeleton"
-
+import { useUpdateRunningMutation } from "@features/running/model/useUpdateRunningMutation"
+import { useInsertRunningMutation } from "@features/running/model/useInsertRunningMutation"
+import { Running } from "@entities/running/constant/running"
 const RunningModal = () => {
   const { runningModal, closeRunningModal } = useRunningModal()
+  const { mutate: updateRunning } = useUpdateRunningMutation()
+  const { mutate: insertRunning } = useInsertRunningMutation()
   return <Modal isOpen={Boolean(runningModal.type)}>
     <EnumRender
       state={runningModal.type ?? 'null'}
@@ -17,8 +21,12 @@ const RunningModal = () => {
         detail: () => runningModal.running && <AsyncBoundary pendingFallback={<RunningListSkeleton />}>
           <SuspenseRunningDetail id={runningModal.running?.id} onClickClose={closeRunningModal} />
         </AsyncBoundary>,
-        insert: () => <RunningForm onClose={closeRunningModal} onSubmit={() => { }} />,
-        update: () => runningModal.running && <RunningForm running={runningModal.running} onClose={closeRunningModal} onSubmit={() => { }} />,
+        insert: () => <RunningForm onClose={closeRunningModal} onSubmit={insertRunning} />,
+        update: () => runningModal.running && <RunningForm running={runningModal.running} onClose={closeRunningModal} onSubmit={(running) => {
+          if (running.id) {
+            updateRunning(running as Running)
+          }
+        }} />,
         null: () => null
       }}
     />
